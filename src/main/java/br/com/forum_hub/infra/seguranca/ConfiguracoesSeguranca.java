b755a27f1ad5b5c8a89f004f36b6b170e057196f
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2026-04-28 13:56:01 -0300

    Aula 03
    Deixando a API stateless

--- src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
+++ src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
@@ -2,13 +2,24 @@ package br.com.forum_hub.infra.seguranca;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesSeguranca {
+    @Bean
+    public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+
     @Bean
     public PasswordEncoder encriptador(){
         return new BCryptPasswordEncoder();
