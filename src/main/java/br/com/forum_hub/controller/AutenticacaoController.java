7d5ad0385345b89f1f92f12be676a382dcf6cb08
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2026-04-28 14:10:10 -0300

    Aula 04
    Configurando o login da API

---
+++ src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
@@ -0,0 +1,24 @@
+package br.com.forum_hub.controller;
+
+import br.com.forum_hub.domain.autenticacao.DadosLogin;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AutenticacaoController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity<Authentication> efetuarLogin(@Valid DadosLogin dados){
+        var autenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+        var authentication = authenticationManager.authenticate(autenticationToken);
+        return ResponseEntity.ok(authentication);
+    }
+}
--- src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
+++ src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
@@ -2,6 +2,8 @@ package br.com.forum_hub.infra.seguranca;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,5 +25,10 @@ public class ConfiguracoesSeguranca {
     @Bean
     public PasswordEncoder encriptador(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
