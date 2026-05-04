1b09981d638895e0fd9f0c7ee00392b661bcd08a
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2026-05-04 16:59:02 -0300

    Aula 07
    Criando um filtro para o Token de acesso

--- src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
+++ src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
@@ -1,5 +1,6 @@
 package br.com.forum_hub.infra.seguranca;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,15 +11,20 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesSeguranca {
+    @Autowired
+    private FiltroTokenAcesso filtroTokenAcesso;
+
     @Bean
     public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
         return http
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf -> csrf.disable())
+                .addFilterBefore(filtroTokenAcesso, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
