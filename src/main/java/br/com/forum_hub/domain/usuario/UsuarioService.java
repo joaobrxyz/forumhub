e2a25454a5f1a2dd33011fb7297c35d016f36155
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2026-05-05 15:16:05 -0300

    Aula 14
    Registrando um usuário

--- src/main/java/br/com/forum_hub/domain/usuario/UsuarioService.java
+++ src/main/java/br/com/forum_hub/domain/usuario/UsuarioService.java
@@ -1,9 +1,12 @@
 package br.com.forum_hub.domain.usuario;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +14,18 @@ public class UsuarioService implements UserDetailsService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return usuarioRepository.findByEmailIgnoreCase(username).orElseThrow(() -> new UsernameNotFoundException("O usuário não foi encontrado!"));
+    }
+
+    @Transactional
+    public Usuario cadastrar(@Valid DadosCadastroUsuario dados) {
+        var senhaCriptografada = passwordEncoder.encode(dados.senha());
+        var usuario = new Usuario(dados, senhaCriptografada);
+        return usuarioRepository.save(usuario);
     }
 }
