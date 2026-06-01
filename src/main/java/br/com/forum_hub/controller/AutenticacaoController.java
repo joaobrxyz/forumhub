d54eb0ffaaba41e69b59905d942427d428b833a0
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2026-06-01 15:21:30 -0300

    Aula 40
    Confirmando o segundo fator

--- src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
+++ src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
@@ -1,19 +1,19 @@
 package br.com.forum_hub.controller;
 
-import br.com.forum_hub.domain.autenticacao.DadosLogin;
-import br.com.forum_hub.domain.autenticacao.DadosRefreshToken;
-import br.com.forum_hub.domain.autenticacao.DadosToken;
-import br.com.forum_hub.domain.autenticacao.TokenService;
+import br.com.forum_hub.domain.autenticacao.*;
 import br.com.forum_hub.domain.usuario.Usuario;
 import br.com.forum_hub.domain.usuario.UsuarioRepository;
+import br.com.forum_hub.infra.seguranca.totp.TotpService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +27,9 @@ public class AutenticacaoController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private TotpService totpService;
+
     @PostMapping("/login")
     public ResponseEntity<DadosToken> efetuarLogin(@Valid @RequestBody DadosLogin dados){
         var autenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
@@ -37,6 +40,18 @@ public class AutenticacaoController {
             return ResponseEntity.ok(new DadosToken(null, null, true));
         }
 
+        String tokenAcesso = tokenService.gerarToken(usuario);
+        String refreshToken = tokenService.gerarRefreshToken(usuario);
+        return ResponseEntity.ok(new DadosToken(tokenAcesso, refreshToken, false));
+    }
+
+    @PostMapping("/verificar-a2f")
+    public ResponseEntity<DadosToken> verificarSegundoFator(@Valid @RequestBody DadosA2F dadosA2F){
+        var usuario = usuarioRepository.findByEmailIgnoreCaseAndVerificadoTrueAndAtivoTrue(dadosA2F.email()).orElseThrow();
+        var codigoValido = totpService.verificarCodigo(dadosA2F.codigo(), usuario);
+        if (!codigoValido){
+            throw new BadCredentialsException("Código inválido");
+        }
         String tokenAcesso = tokenService.gerarToken(usuario);
         String refreshToken = tokenService.gerarRefreshToken(usuario);
         return ResponseEntity.ok(new DadosToken(tokenAcesso, refreshToken, false));
